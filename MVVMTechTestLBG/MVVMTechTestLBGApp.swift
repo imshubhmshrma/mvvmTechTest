75778e87dc3891ea8be15511b5be134c1a6fcfb6
imshubhmshrma <imshubhmshrma@gmail.com>
2026-01-07 10:50:22 +0530

    Initial Commit

---
+++ MVVMTechTestLBG/MVVMTechTestLBGApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMTechTestLBGApp.swift
+//  MVVMTechTestLBG
+//
+//  Created by Shubham Sharma on 07/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MVVMTechTestLBGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MVVMTechTestLBGTests/MVVMTechTestLBGTests.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMTechTestLBGTests.swift
+//  MVVMTechTestLBGTests
+//
+//  Created by Shubham Sharma on 07/01/26.
+//
+
+import Testing
+@testable import MVVMTechTestLBG
+
+struct MVVMTechTestLBGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
